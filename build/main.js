7fe35c3444bfad71c8c4743eb1fe13ebe27b6469
Ruzell11 <johnruzell123@gmail.com>
2023-08-06 09:44:05 +0800

    compile es5 to es6 build using babel

---
+++ build/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var app = (0, _express["default"])();
+var port = 3000;
+app.get('/', function (req, res) {
+  res.send('<h2>Hello Worlsd</h2>');
+});
+app.listen(port, function () {
+  return console.log("Listening on port ".concat(port));
+});
