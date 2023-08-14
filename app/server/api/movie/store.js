d801e1a27a593107e683e5dbc3425bba1b85e898
Ruzell11 <johnruzell123@gmail.com>
2023-08-14 16:20:58 +0800

    modify prod docker-compose

--- app/server/api/movie/store.js
+++ app/server/api/movie/store.js
@@ -1,6 +1,6 @@
 import { redisClient } from '../../service/redis';
 const getMovieList = async (req, res) => {
-    res.json({ message: "Movie routes is working fine edited" })
+    res.json({ message: "Movie routes is working fine just change" })
 }
 
 export default { getMovieList }
