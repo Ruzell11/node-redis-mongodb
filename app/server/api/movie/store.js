70507097ab109a15f3e47682335812adde57e5ef
Ruzell11 <johnruzell123@gmail.com>
2023-08-14 15:43:00 +0800

    edit store movie

--- app/server/api/movie/store.js
+++ app/server/api/movie/store.js
@@ -1,13 +1,6 @@
 import { redisClient } from '../../service/redis';
 const getMovieList = async (req, res) => {
-    await redisClient.hSet('user-session:123', {
-        name: 'John',
-        surname: 'Smith',
-        company: 'Redis',
-        age: 29
-    })
-    console.log("ITS RUNNING")
-    res.json({ message: "Movie routes is working fine" })
+    res.json({ message: "Movie routes is working fine edited" })
 }
 
 export default { getMovieList }
